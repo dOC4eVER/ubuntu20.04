d3bf5217818c0e005b08a94577b6c584840b137d
dOC4eVER <145916107+dOC4eVER@users.noreply.github.com>
2023-11-11 14:53:01 +0100

    Update start_services.sh
    deletion of call to pytools/ directory, which causes error when loading start_services.sh, due to non-existent directory for configuring config.py

--- start_services.sh
+++ start_services.sh
@@ -17,7 +17,7 @@ chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb 2>/dev/null
 wget -qO /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb https://bitbucket.org/emre1393/xtreamui_mirror/downloads/GeoLite2.mmdb 2>/dev/null
 chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb 2>/dev/null
 geoliteversion=$(wget -qO- https://github.com/dOC4eVER/ubuntu20.04/releases/download/start/Geolite2_status.json | jq -r ".version")
-PASSMYSQL=$(python2 /home/xtreamcodes/iptv_xtream_codes/pytools/config.py DECRYPT | grep Password | sed "s|Password:            ||g")
+PASSMYSQL=$(python2 /home/xtreamcodes/iptv_xtream_codes/config.py DECRYPT | grep Password | sed "s|Password:            ||g")
 mysql -u user_iptvpro -p$PASSMYSQL -P 7999 xtream_iptvpro -e "UPDATE admin_settings SET value = '$geoliteversion' WHERE admin_settings.type = 'geolite2_version'; " 2>/dev/null
 chown -R xtreamcodes:xtreamcodes /sys/class/net 2>/dev/null
 chown -R xtreamcodes:xtreamcodes /home/xtreamcodes 2>/dev/null
